a79923053943321f8a6580c18b7ca0a5e8caf88c
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-03-18 18:49:21 +0100

    fix: duck emoji getter

--- utils/getEmojiPairByDuckName.ts
+++ utils/getEmojiPairByDuckName.ts
@@ -1,9 +1,12 @@
 import { Client } from "discord.js";
 import getAvailableEmojis from "./getAvailableEmojis";
+import convertDuckNameToId from "./convertDuckNameToId";
 
 const getEmojiPairByDuckName = (client: Client, duckName: string) => {
   const emojis = getAvailableEmojis(client);
-  const head = emojis.find((emoji) => emoji.split(":")[1] === duckName);
+  const head = emojis.find(
+    (emoji) => emoji.split(":")[1] === convertDuckNameToId(duckName)
+  );
   const body = emojis.find(
     (emoji) => emoji.split(":")[1] === duckName + "_body"
   );
